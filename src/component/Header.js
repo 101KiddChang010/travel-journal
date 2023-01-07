b8fce880db8a4f9792e46d6f4e74c9821506f64d
Kidd <kiddochang02@gmail.com>
2023-01-07 18:50:10 -0500

    Create header

---
+++ src/component/Header.js
@@ -0,0 +1,10 @@
+import '../App.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <i class="fa-solid fa-earth-americas fa-3x"></i>
+            <h1>My Travel Journal</h1>
+        </div>
+    )
+}
