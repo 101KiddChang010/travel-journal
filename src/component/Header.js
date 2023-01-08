ddf97023ab43aabd101b496079ee32953d243c02
Kidd <kiddochang02@gmail.com>
2023-01-07 20:34:09 -0500

    Change icon

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import '../App.css';
 export default function Header() {
     return (
         <div className="header">
-            <i class="fa-solid fa-earth-americas fa-3x"></i>
+            <i class="fa-solid fa-plane fa-2x"></i>
             <h1>My Travel Journal</h1>
         </div>
     )
