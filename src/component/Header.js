1e27c9782c51fbe8d700ad2c399eebae9020b6ad
Kidd <kiddochang02@gmail.com>
2023-01-08 01:18:24 -0500

    Fix class -> className

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import '../App.css';
 export default function Header() {
     return (
         <div className="header">
-            <i class="fa-solid fa-plane fa-2x"></i>
+            <i className="fa-solid fa-plane fa-2x"></i>
             <h2>My Travel Journal</h2>
         </div>
     )
